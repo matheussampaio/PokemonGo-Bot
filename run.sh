df03219bfc6f046bc170f5216cf679ef3c676817
Matheus Sampaio <matheus1401@gmail.com>
2016-08-08 00:19:47 -0500

    easy exec

--- run.sh
+++ run.sh
@@ -1,18 +1,10 @@
 #!/usr/bin/env bash
 
 # Starts PokemonGo-Bot
-config=""
+virtualenv .
 
-if [ ! -z $1 ]; then
-    config=$1
-else
-    config="./configs/config.json"
-    if [ ! -f ${config} ]; then
-        echo -e "There's no ./configs/config.json file"
-        echo -e "Please create one or use another config file"
-        echo -e "./run.sh [path/to/config/file]"
-        exit 1
-    fi
-fi
+source bin/active
 
-python pokecli.py --config ${config}
+pip install -r requirements.txt
+
+python pokecli.py -cf configs/config.json
